1d6b4231f76f3edd4926243adb9cf3363764d685
domi <01152470@pw.edu.pl>
2024-07-08 19:16:02 +0200

    add backtracking algorithm

---
+++ game.js
@@ -0,0 +1,61 @@
+import { Util } from './Util.js';
+
+let board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+function solve(grid) {
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            if (grid[row][col] === 0) {
+                for (let num = 1; num <= 9; num++) {
+                    if (isValid(grid, row, col, num)) {
+                        grid[row][col] = num;
+                        if (solve(grid)) {
+                            return true;
+                        }
+                        grid[row][col] = 0;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function isValid(grid, row, col, num) {
+    for (let x = 0; x < 9; x++) {
+        if (grid[row][x] === num) {
+            return false;
+        }
+    }
+    for (let x = 0; x < 9; x++) {
+        if (grid[x][col] === num) {
+            return false;
+        }
+    }
+    let startRow = row - row % 3;
+    let startCol = col - col % 3;
+    for (let i = startRow; i < startRow + 3; i++) {
+        for (let j = startCol; j < startCol + 3; j++) {
+            if (grid[i][j] === num) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+let solution = [];
+Util.copyGrid(board, solution);
+solve(solution);
+Util.print2DArray(board);
